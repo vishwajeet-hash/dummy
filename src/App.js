3f9c8cdb7976b423a86db65c0fe9e235ef288d21
vishwajeet singh <v.k.singh220701@gmail.com>
2023-07-09 22:57:53 +0530

    commited

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import About from "./About/About";
+import Contact from './Contact/Contact';
+import Menu from "./Menu";
+import Service from "./Service/Service";
+import PetShop from "./PetShop/PetShop";
+import "./App.css"
+function App() {
+  return (
+    <>
+      <div className="App">
+      
+        <BrowserRouter>
+          <Menu/>
+          <Routes>
+          <Route path="/"  element={<About name="About" />} />
+          <Route path="/service" element={<Service name="Service" />} />
+          <Route path="/pet-shop" element={<PetShop name="pet-shop" />} />
+        <Route path="/contact" element={<Contact name="Contact"/>} />
+      </Routes>
+        </BrowserRouter>
+        </div>
+      </>
+
+  );
+}
+
+export default App;
